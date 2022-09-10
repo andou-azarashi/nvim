bfc2c3707f262219316d92b18d17a66538072398
andou-azarashi <akiyamamio_miku@icloud.com>
2022-09-10 12:30:29 -0400

    trasfer setting from init.vim to lua

--- lua/globals/options.lua
+++ lua/globals/options.lua
@@ -1,6 +1,27 @@
+local o = vim.o
+
 vim.g.mapleader = " "
 
-vim.o.relativenumber = true
-vim.o.number = true
+o.encoding = "utf-8"
+o.pumheight = 10
+o.fileencoding = "utf-8"
+o.hidden = true
+o.relativenumber = true
+o.number = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+o.wrap = false
+o.smartcase = true
+o.backup = false
+o.swapfile = false
+o.writebackup = false
+o.updatetime = 300
+o.clipboard = 'unnamedplus'
+o.autochdir = true
+o.incsearch = true
+o.cursorline = true
 
 require "packer_plugins"
