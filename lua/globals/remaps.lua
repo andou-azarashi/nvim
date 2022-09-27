ee19e5adf9b7ebe373c168d1c40a3592e28aadf8
andou-azarashi <akiyamakazuya_miku@icloud.com>
2022-09-27 18:40:27 +0900

    change jk to jj

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -5,7 +5,7 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', options)
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', options)
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', options)
 
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', options)
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', options)
 
 vim.api.nvim_set_keymap('n', '<leader>m', '<cmd>NvimTreeToggle<cr>', {desc = 'Tree'})
 vim.api.nvim_set_keymap('n', '<leader>;', '<cmd>w<cr>', {desc = 'Save'})
